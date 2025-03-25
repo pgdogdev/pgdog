af9684cfd1ed625a281e23ebfeb64be755b934d9
Lev Kokotov <lev.kokotov@gmail.com>
2025-03-24 22:35:51 -0700

    commit

--- pgdog/tests/vector/measure_recall.py
+++ pgdog/tests/vector/measure_recall.py
@@ -1,15 +1,25 @@
 import psycopg
+import json
+
+with open("test_embeddings.json") as f:
+    embeddings = json.load(f)
 
 if __name__ == "__main__":
-    conn = psycopg.connect("user=pgdog password=pgdog dbname=pgdog_sharded host=127.0.0.1 port=6432")
+    conn = psycopg.connect("user=pgdog password=pgdog dbname=pgdog_sharded host=127.0.0.1 port=6432") # change dbname to pgdog to get ground truth
     cur = conn.cursor()
 
-    cur.execute("SELECT embedding FROM embeddings LIMIT 100")
-    embeddings = cur.fetchall()
-    hits = []
+    results = []
     for embedding in embeddings:
         vec = str(embedding[0])
         cur.execute("SELECT embedding FROM embeddings WHERE embedding <-> %s < 0.1 ORDER BY embedding <-> %s LIMIT 5", (vec,vec,))
         neighbors = cur.fetchall()
-        hits.append(len(neighbors))
-    print(hits)
+        results.append(len(neighbors))
+
+    hits = 0
+    misses = 0
+    for h in results:
+        if h > 0:
+            hits += 1
+        else:
+            misses += 1
+    print(f"hits: {hits}, misses: {misses}")
