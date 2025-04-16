860a2e097559e9204c6bdfc7b5711e70df63751e
Lev Kokotov <lev.kokotov@gmail.com>
2025-04-16 11:30:32 -0700

    Optional prepared statements

--- examples/gel/gel.sh
+++ examples/gel/gel.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 docker run \
     -e GEL_SERVER_SECURITY=insecure_dev_mode \
-    -e GEL_SERVER_BACKEND_DSN=postgres://pgdog:pgdog@127.0.0.1:6432/pgdog \
+    -e GEL_SERVER_BACKEND_DSN=postgres://pgdog:pgdog@127.0.0.1:5432/pgdog \
     --network=host \
     geldata/gel:latest
--- pgdog/src/frontend/client/mod.rs
+++ pgdog/src/frontend/client/mod.rs
@@ -296,7 +296,7 @@ impl Client {
             let request = Request::new(self.id);
             match inner.connect(&request).await {
                 Ok(()) => {
-                    inner.backend.sync_params(&self.params).await?;
+                    inner.backend.link_client(&self.params).await?;
                 }
                 Err(err) => {
                     if err.no_server() {
@@ -432,11 +432,15 @@ impl Client {
             if message.code() == 'X' {
                 return Ok(vec![].into());
             } else {
-                let message = ProtocolMessage::from_bytes(message.to_bytes()?)?;
-                if prepared_statements && message.extended() {
-                    buffer.push(self.prepared_statements.maybe_rewrite(message)?);
+                if prepared_statements {
+                    let message = ProtocolMessage::from_bytes(message.to_bytes()?)?;
+                    if message.extended() {
+                        buffer.push(self.prepared_statements.maybe_rewrite(message)?);
+                    } else {
+                        buffer.push(message);
+                    }
                 } else {
-                    buffer.push(message)
+                    buffer.push(message.into())
                 }
             }
         }
