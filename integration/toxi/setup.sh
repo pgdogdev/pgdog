f279d2be7cb7d12516484e70d9ba937c86fe1c34
Lev Kokotov <lev.kokotov@gmail.com>
2025-04-17 17:40:18 -0700

    Here we go with CI again

--- integration/toxi/setup.sh
+++ integration/toxi/setup.sh
@@ -3,6 +3,10 @@ set -e
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 CLI="$SCRIPT_DIR/../toxiproxy-cli"
 
+killall toxiproxy-server || true
+${SCRIPT_DIR}/../toxiproxy-server &
+sleep 1
+
 ${CLI} delete primary || true
 ${CLI} delete replica || true
 ${CLI} create --listen :5435 --upstream :5432 primary
