f38cd5bba3a0f1f502927b8fa7b03c1d31d9caac
Lev Kokotov <lev.kokotov@gmail.com>
2025-05-07 09:36:17 -0700

    Add Go to CI

--- integration/go/run.sh
+++ integration/go/run.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-go get
-go test -count 3
+set -e
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+source ${SCRIPT_DIR}/../common.sh
+
+run_pgdog
+wait_for_pgdog
+
+bash ${SCRIPT_DIR}/dev.sh
+
+stop_pgdog
