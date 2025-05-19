e3295465ab89744c22e124e992db1595fdbbe9c3
Lev Kokotov <lev.kokotov@gmail.com>
2025-05-19 11:26:54 -0700

    Rewrite anonymous statements to work with go

--- integration/go/go_pq/dev.sh
+++ integration/go/go_pq/dev.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-set -e
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
 pushd ${SCRIPT_DIR}
-bash go_pgx/run.sh
-bash go_pq/run.sh
+
+go get
+go test -count 3
+
 popd
