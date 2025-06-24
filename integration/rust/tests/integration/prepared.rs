05aad918e1c3ca58408af221cab12bfefa4d3118
Lev Kokotov <lev.kokotov@gmail.com>
2025-06-24 13:34:40 -0700

    remove ddead

--- integration/rust/tests/integration/prepared.rs
+++ integration/rust/tests/integration/prepared.rs
@@ -1,13 +1,1 @@
-use rust::setup::{admin_sqlx, connections_sqlx};
-use serial_test::serial;
-use sqlx::{Executor, Pool, Postgres, Row};
 
-#[tokio::test]
-async fn test_prepared_counter() {
-    let conns = connections_sqlx().await;
-    let admin = admin_sqlx().await;
-
-    for conn in conns {
-        let prepared = conn.prepare("SELECT $1").await.unwrap();
-    }
-}
