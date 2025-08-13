2f61e16392f1729b3f2c6171377fc9d6b42c1418
Nic Laflamme <nic@laflamme.io>
2025-08-13 11:31:45 -0400

    wip

--- pgdog/src/frontend/router/parser/query/mod.rs
+++ pgdog/src/frontend/router/parser/query/mod.rs
@@ -24,6 +24,7 @@ mod explain;
 mod select;
 mod set;
 mod shared;
+mod show;
 mod transaction;
 mod update;
 
@@ -312,22 +313,6 @@ impl QueryParser {
         }
     }
 
-    /// Handle SHOW command.
-    fn show(
-        &mut self,
-        stmt: &VariableShowStmt,
-        context: &QueryParserContext,
-    ) -> Result<Command, Error> {
-        match stmt.name.as_str() {
-            "pgdog.shards" => Ok(Command::Shards(context.shards)),
-            _ => {
-                let shard = Shard::Direct(round_robin::next() % context.shards);
-                let route = Route::write(shard).set_read(context.read_only);
-                Ok(Command::Query(route))
-            }
-        }
-    }
-
     /// Handle COPY command.
     fn copy(stmt: &CopyStmt, context: &QueryParserContext) -> Result<Command, Error> {
         let parser = CopyParser::new(stmt, context.router_context.cluster)?;
@@ -354,42 +339,3 @@ impl QueryParser {
 
 #[cfg(test)]
 mod test;
-
-#[cfg(test)]
-mod test_show {
-    use crate::backend::Cluster;
-    use crate::frontend::router::parser::Shard;
-    use crate::frontend::router::QueryParser;
-    use crate::frontend::{Buffer, PreparedStatements, RouterContext};
-    use crate::net::messages::Query;
-    use crate::net::Parameters;
-
-    #[test]
-    fn show_runs_on_a_direct_shard_round_robin() {
-        let mut ps = PreparedStatements::default();
-        let c = Cluster::new_test();
-        let p = Parameters::default();
-        let mut parser = QueryParser::default();
-
-        // First call
-        let query = "SHOW TRANSACTION ISOLATION LEVEL";
-        let buffer = Buffer::from(vec![Query::new(query).into()]);
-        let context = RouterContext::new(&buffer, &c, &mut ps, &p, false).unwrap();
-
-        let first = parser.parse(context).unwrap().clone();
-        let first_shard = first.route().shard();
-        assert!(matches!(first_shard, Shard::Direct(_)));
-
-        // Second call
-        let query = "SHOW TRANSACTION ISOLATION LEVEL";
-        let buffer = Buffer::from(vec![Query::new(query).into()]);
-        let context = RouterContext::new(&buffer, &c, &mut ps, &p, false).unwrap();
-
-        let second = parser.parse(context).unwrap().clone();
-        let second_shard = second.route().shard();
-        assert!(matches!(second_shard, Shard::Direct(_)));
-
-        // Round robin shard routing
-        assert!(second_shard != first_shard);
-    }
-}
