851fc8cc0389635d41d3067a8b96d7553964d428
Nic Laflamme <nic@laflamme.io>
2025-08-13 10:20:18 -0400

    wip

--- pgdog/src/frontend/router/parser/query/mod.rs
+++ pgdog/src/frontend/router/parser/query/mod.rs
@@ -320,9 +320,12 @@ impl QueryParser {
     ) -> Result<Command, Error> {
         match stmt.name.as_str() {
             "pgdog.shards" => Ok(Command::Shards(context.shards)),
-            _ => Ok(Command::Query(
-                Route::write(Shard::All).set_read(context.read_only),
-            )),
+            _ => {
+                let shard = Shard::Direct(round_robin::next() % context.shards);
+                let route = Route::write(shard).set_read(context.read_only);
+                let command = Command::Query(route);
+                Ok(command)
+            }
         }
     }
 
@@ -352,3 +355,42 @@ impl QueryParser {
 
 #[cfg(test)]
 mod test;
+
+#[cfg(test)]
+mod test_show {
+    use crate::backend::Cluster;
+    use crate::frontend::router::parser::Shard;
+    use crate::frontend::router::QueryParser;
+    use crate::frontend::{Buffer, PreparedStatements, RouterContext};
+    use crate::net::messages::Query;
+    use crate::net::Parameters;
+
+    #[test]
+    fn show_runs_on_a_direct_shard_round_robin() {
+        let mut ps = PreparedStatements::default();
+        let c = Cluster::new_test();
+        let p = Parameters::default();
+        let mut parser = QueryParser::default();
+
+        // First call
+        let query = "SHOW TRANSACTION ISOLATION LEVEL";
+        let buffer = Buffer::from(vec![Query::new(query).into()]);
+        let context = RouterContext::new(&buffer, &c, &mut ps, &p, false).unwrap();
+
+        let first = parser.parse(context).unwrap().clone();
+        let first_shard = first.route().shard();
+        assert!(matches!(first_shard, Shard::Direct(_)));
+
+        // Second call
+        let query = "SHOW TRANSACTION ISOLATION LEVEL";
+        let buffer = Buffer::from(vec![Query::new(query).into()]);
+        let context = RouterContext::new(&buffer, &c, &mut ps, &p, false).unwrap();
+
+        let second = parser.parse(context).unwrap().clone();
+        let second_shard = second.route().shard();
+        assert!(matches!(second_shard, Shard::Direct(_)));
+
+        // Round robin shard routing
+        assert!(second_shard != first_shard);
+    }
+}
