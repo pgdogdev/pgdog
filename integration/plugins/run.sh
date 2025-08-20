eaf2ee5d3b8182c27f444b0c10bc32b449c67880
Lev Kokotov <lev.kokotov@gmail.com>
2025-08-19 21:12:37 -0700

    save

--- integration/plugins/run.sh
+++ integration/plugins/run.sh
@@ -7,6 +7,9 @@ pushd ${SCRIPT_DIR}/../../plugins/pgdog-example-plugin
 cargo build --release
 popd
 
+export LD_LIBRARY_PATH=${SCRIPT_DIR}/../../target/release
+export DYLD_LIBRARY_PATH=${LD_LIBRARY_PATH}
+
 run_pgdog $SCRIPT_DIR
 wait_for_pgdog
 
