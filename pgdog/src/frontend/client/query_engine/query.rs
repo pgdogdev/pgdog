f50456a5db321c1cd2a64c832624215de0033a4e
Nic Laflamme <nic@laflamme.io>
2025-08-21 18:49:03 -0400

    rever-change-delta

--- pgdog/src/frontend/client/query_engine/query.rs
+++ pgdog/src/frontend/client/query_engine/query.rs
@@ -98,11 +98,9 @@ impl QueryEngine {
             let in_transaction = message.in_transaction() || self.begin_stmt.is_some();
             if !in_transaction {
                 context.transaction = None;
-            }
-
-            // Query parser is disabled, so the server is responsible for telling
-            // us we started a transaction. Do not override an existing transaction state.
-            if in_transaction && context.transaction.is_none() {
+            } else if context.transaction.is_none() {
+                // Query parser is disabled, so the server is responsible for telling us
+                // we started a transaction.
                 context.transaction = Some(TransactionType::ReadWrite);
             }
 
