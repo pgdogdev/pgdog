c6d7328d2e16b8e684873415ac0eef98b5b3bd02
Justin George <justin.george@gmail.com>
2025-09-25 12:33:33 -0700

    try stopping between sub-suites

--- integration/load_balancer/run.sh
+++ integration/load_balancer/run.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 set -e
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+source ${SCRIPT_DIR}/../common.sh
 
 pushd ${SCRIPT_DIR}
 
 export PGUSER=postgres
-
 export PGHOST=127.0.0.1
 export PGDATABASE=postgres
 export PGPASSWORD=postgres
 
 docker-compose up -d
 
-
 echo "Waiting for Postgres to be ready"
-
 for p in 45000 45001 45002; do
     export PGPORT=${p}
     while ! pg_isready; do
@@ -22,24 +20,7 @@ for p in 45000 45001 45002; do
     done
 done
 
-
-pushd ${SCRIPT_DIR}/../../
-REPO_ROOT=$(pwd)
-popd
-
-PGDOG_BIN_PATH="${PGDOG_BIN:-}"
-if [ -z "${PGDOG_BIN_PATH}" ]; then
-    pushd ${REPO_ROOT}
-    cargo build --release
-    PGDOG_BIN_PATH="$(pwd)/target/release/pgdog"
-    popd
-fi
-
-sleep 2
-
-"${PGDOG_BIN_PATH}" \
-    --config ${SCRIPT_DIR}/pgdog.toml \
-    --users ${SCRIPT_DIR}/users.toml &
+run_pgdog ${SCRIPT_DIR}
 
 export PGPORT=6432
 while ! pg_isready; do
@@ -51,6 +32,7 @@ go get
 go test -v -count 3
 popd
 
-killall pgdog
+stop_pgdog
 
 docker-compose down
+popd
