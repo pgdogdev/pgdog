d2c6f7aa24a5a74db93aaae6a656eb3e6e200678
Justin George <justin.george@gmail.com>
2025-09-29 15:01:22 -0700

    skip cloning name if passed by reference

--- pgdog/src/frontend/client_request.rs
+++ pgdog/src/frontend/client_request.rs
@@ -180,8 +180,7 @@ impl ClientRequest {
         for message in self.messages.iter_mut() {
             if let ProtocolMessage::Parse(parse) = message {
                 parse.set_query(query);
-                let name = parse.name();
-                prepared.update_and_set_rewrite_plan(&name, query, plan.clone());
+                prepared.update_and_set_rewrite_plan(&parse.name(), query, plan.clone());
                 updated = true;
             }
         }
