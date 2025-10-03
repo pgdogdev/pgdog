e5ce9ea4cf76d264b47be1651784cfa812ca909f
Lev Kokotov <lev.kokotov@gmail.com>
2025-10-03 09:12:06 -0700

    Add to CI

--- integration/schema_sync/dev.sh
+++ integration/schema_sync/dev.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+PGDOG_BIN_PATH="${PGDOG_BIN:-${SCRIPT_DIR}/../../target/release/pgdog}"
 pushd ${SCRIPT_DIR}
 
 export PGPASSWORD=pgdog
@@ -8,20 +9,18 @@ export PGUSER=pgdog
 psql -f ${SCRIPT_DIR}/ecommerce_schema.sql pgdog1
 psql -c 'CREATE PUBLICATION pgdog FOR ALL TABLES' pgdog1 || true
 
-cargo run \
-    --bin pgdog \
+${PGDOG_BIN_PATH} \
     schema-sync \
     --from-database source \
     --to-database destination \
     --publication pgdog
 
-cargo run -- \
+${PGDOG_BIN_PATH} \
     schema-sync \
     --from-database source \
     --to-database destination \
     --publication pgdog \
     --data-sync-complete
-
 
 pg_dump \
     --schema-only \
