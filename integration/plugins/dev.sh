507256785998412b8cc3a0f1ef49025c8f00dd76
Justin George <justin.george@gmail.com>
2025-10-07 10:05:52 -0700

    use vendored bundler

--- integration/plugins/dev.sh
+++ integration/plugins/dev.sh
@@ -2,6 +2,7 @@
 set -e
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 pushd ${SCRIPT_DIR}
-bundle
+bundle config set --local path vendor/bundle
+bundle install
 bundle exec rspec *_spec.rb
 popd
