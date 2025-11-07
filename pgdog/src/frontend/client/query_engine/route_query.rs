f5cb03bfaa56d5454e6887628a86d3d626f01a01
Lev Kokotov <lev.kokotov@gmail.com>
2025-11-06 16:40:56 -0800

    Config reload was delayed by oen transaction

--- pgdog/src/frontend/client/query_engine/route_query.rs
+++ pgdog/src/frontend/client/query_engine/route_query.rs
@@ -9,7 +9,32 @@ impl QueryEngine {
     ) -> Result<bool, Error> {
         // Admin doesn't have a cluster.
         let cluster = if let Ok(cluster) = self.backend.cluster() {
-            cluster
+            if !cluster.online() {
+                let identifier = cluster.identifier();
+
+                // Reload cluster config.
+                self.backend.reload()?;
+
+                match self.backend.cluster() {
+                    Ok(cluster) => cluster,
+                    Err(err) => {
+                        // Cluster is gone.
+                        error!("{:?} [{:?}]", err, context.stream.peer_addr());
+                        let error =
+                            ErrorResponse::connection(&identifier.user, &identifier.database);
+                        self.hooks.on_engine_error(context, &error)?;
+
+                        let bytes_sent = context
+                            .stream
+                            .error(error, context.in_transaction())
+                            .await?;
+                        self.stats.sent(bytes_sent);
+                        return Ok(false);
+                    }
+                }
+            } else {
+                cluster
+            }
         } else {
             return Ok(true);
         };
