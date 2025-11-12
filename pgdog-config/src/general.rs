20791d6bb735e68054d60ab97f75cfe60d763bb3
Michael Hauser-Raspe <michael.hauser-raspe@cantab.net>
2025-11-12 16:47:57 +0000

    Create query_parser_enabled configuration option.
    This option allows you to enable the query parser, in cases when the default
    setting would be off.
    Currently the default setting for when queries would be parsed is when we either:
    * have a primary and a replica.
    * have more than one shard.
    * have prepared_statements = 'full'
    
    However there are cases where you might not want to have any of these, but still
    want to have your queries be parsed. So that, for instance, you can have client
    state (as in your `SET` commands) properly tracked across different clients.

--- pgdog-config/src/general.rs
+++ pgdog-config/src/general.rs
@@ -91,6 +91,8 @@ pub struct General {
     /// Prepared statatements support.
     #[serde(default)]
     pub prepared_statements: PreparedStatements,
+    /// Parse Queries override.
+    pub query_parser_enabled: bool,
     /// Limit on the number of prepared statements in the server cache.
     #[serde(default = "General::prepared_statements_limit")]
     pub prepared_statements_limit: usize,
@@ -198,6 +200,7 @@ impl Default for General {
             openmetrics_port: Self::openmetrics_port(),
             openmetrics_namespace: Self::openmetrics_namespace(),
             prepared_statements: Self::prepared_statements(),
+            query_parser_enabled: Self::query_parser_enabled(),
             prepared_statements_limit: Self::prepared_statements_limit(),
             query_cache_limit: Self::query_cache_limit(),
             passthrough_auth: Self::default_passthrough_auth(),
@@ -398,6 +401,10 @@ impl General {
 
     fn prepared_statements() -> PreparedStatements {
         Self::env_enum_or_default("PGDOG_PREPARED_STATEMENTS")
+    }
+
+    fn query_parser_enabled() -> bool {
+        Self::env_bool_or_default("PGDOG_QUERY_PARSER_ENABLED", false)
     }
 
     fn auth_type() -> AuthType {
