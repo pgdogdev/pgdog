7dc26cc2124cb86fad8e743ac0e81ee61f4d95ca
Lev Kokotov <lev.kokotov@gmail.com>
2025-12-08 07:05:11 -0800

    Add explain output back

--- pgdog/src/frontend/router/parser/query/delete.rs
+++ pgdog/src/frontend/router/parser/query/delete.rs
@@ -1,6 +1,4 @@
-use crate::frontend::router::parser::where_clause::TablesSource;
-
-use super::shared::ConvergeAlgorithm;
+use super::StatementParser;
 use super::*;
 
 impl QueryParser {
@@ -9,47 +7,32 @@ impl QueryParser {
         stmt: &DeleteStmt,
         context: &QueryParserContext,
     ) -> Result<Command, Error> {
-        let table = stmt.relation.as_ref().map(Table::from);
+        let shard = StatementParser::from_delete(
+            stmt,
+            context.router_context.bind,
+            &context.sharding_schema,
+            self.recorder_mut(),
+        )
+        .shard()?;
 
-        if let Some(table) = table {
-            // Schema-based sharding.
-            if let Some(schema) = context.sharding_schema.schemas.get(table.schema()) {
-                let shard: Shard = schema.shard().into();
-
-                if let Some(recorder) = self.recorder_mut() {
-                    recorder.record_entry(
-                        Some(shard.clone()),
-                        format!("DELETE matched schema {}", schema.name()),
-                    );
-                }
-
-                return Ok(Command::Query(Route::write(shard)));
-            }
-
-            let source = TablesSource::from(table);
-            let where_clause = WhereClause::new(&source, &stmt.where_clause);
-
-            if let Some(where_clause) = where_clause {
-                let shards = Self::where_clause(
-                    &context.sharding_schema,
-                    &where_clause,
-                    context.router_context.bind,
-                    &mut self.explain_recorder,
-                )?;
-                let shard = Self::converge(&shards, ConvergeAlgorithm::default());
+        let shard = match shard {
+            Some(shard) => {
                 if let Some(recorder) = self.recorder_mut() {
                     recorder.record_entry(
                         Some(shard.clone()),
                         "DELETE matched WHERE clause for sharding key",
                     );
                 }
-                return Ok(Command::Query(Route::write(shard)));
+                shard
             }
-        }
+            None => {
+                if let Some(recorder) = self.recorder_mut() {
+                    recorder.record_entry(None, "DELETE fell back to broadcast");
+                }
+                Shard::default()
+            }
+        };
 
-        if let Some(recorder) = self.recorder_mut() {
-            recorder.record_entry(None, "DELETE fell back to broadcast");
-        }
-        Ok(Command::Query(Route::write(None)))
+        Ok(Command::Query(Route::write(shard)))
     }
 }
