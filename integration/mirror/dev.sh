c6825e8c291261609fdc6077689d5c98138422b2
Costi <comisarulmoldovan@gmail.com>
2026-03-13 15:23:18 -0500

    Add PHP mirror integration coverage

--- integration/mirror/dev.sh
+++ integration/mirror/dev.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
+set -euo pipefail
+
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 pushd ${SCRIPT_DIR}/ruby
-bundle exec *_spec.rb
+export GEM_HOME=~/.gem
+mkdir -p ${GEM_HOME}
+bundle install
+bundle exec rspec *_spec.rb
+popd
+
+pushd ${SCRIPT_DIR}/php
+bash run.sh
 popd
