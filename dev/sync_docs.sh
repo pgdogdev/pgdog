31ba776193e4bc006364d4b3e076713fff0f81e6
Lev Kokotov <lev.kokotov@gmail.com>
2026-03-16 16:58:37 -0700

    chore: update plugins version

--- dev/sync_docs.sh
+++ dev/sync_docs.sh
@@ -6,6 +6,7 @@ cd ${SCRIPT_DIR}/../
 cargo doc
 cd target/doc
 aws s3 sync pgdog_plugin s3://pgdog-docsrs/pgdog_plugin
+aws s3 sync pgdog_macros s3://pgdog-docsrs/pgdog_macros
 aws s3 sync pgdog s3://pgdog-docsrs/pgdog
-aws s3 sync pgdog_plugin_build s3://pgdog-docsrs/pgdog_plugin_build
+aws s3 sync pg_query  s3://pgdog-docsrs/pg_query
 aws s3 cp ${SCRIPT_DIR}/docs_redirect.html s3://pgdog-docsrs/index.html
