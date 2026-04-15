12f5deef5da32a30bf76918f84a96d3266a9caa1
Lev Kokotov <lev.kokotov@gmail.com>
2026-04-15 10:48:35 -0700

    save

--- integration/dev-server.sh
+++ integration/dev-server.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 THIS_SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-# source ${THIS_SCRIPT_DIR}/setup.sh
-# source ${THIS_SCRIPT_DIR}/toxi/setup.sh
+source ${THIS_SCRIPT_DIR}/setup.sh
+source ${THIS_SCRIPT_DIR}/toxi/setup.sh
 pushd ${THIS_SCRIPT_DIR}/../
 export NODE_ID=pgdog-dev-1
 CMD="cargo run -- --config ${THIS_SCRIPT_DIR}/pgdog.toml --users ${THIS_SCRIPT_DIR}/users.toml"
