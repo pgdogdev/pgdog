8d240880adf3ca6d499f163acf881760f41f1fff
Abdessalam ZAIMI <abdessalam.zaimi@sia-partners.com>
2026-05-16 10:19:40 +0200

    fix: password in make_addr

--- pgdog/src/backend/auth/azure_workload_identity.rs
+++ pgdog/src/backend/auth/azure_workload_identity.rs
@@ -94,7 +94,7 @@ mod tests {
             port: 5432,
             database_name: "postgres".into(),
             user: "db_user".into(),
-            passwords: vec![String::new().into()],
+            passwords: vec!["".into()],
             database_number: 0,
             server_auth: ServerAuth::AzureWorkloadIdentity,
             server_iam_region: None,
