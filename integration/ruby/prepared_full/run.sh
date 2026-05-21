8a3ada5d35cf256cb2f725d76a05a35fa2f323bd
Kiryl Mialeshka <8974488+meskill@users.noreply.github.com>
2026-05-21 14:31:08 +0200

    fix(prepared_statements): full rewrites extended as well (#971)
    fixes #604

--- integration/ruby/prepared_full/run.sh
+++ integration/ruby/prepared_full/run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-source "${SCRIPT_DIR}/common.sh"
-dev_suite
+source "${SCRIPT_DIR}/../common.sh"
+install_deps
+run_suite "$SCRIPT_DIR"
