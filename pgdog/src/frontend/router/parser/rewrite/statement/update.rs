771545c0518e9a356fcacd298d432fedbbb49684
Sage Griffin <sage@sagetheprogrammer.com>
2026-07-16 10:02:47 -0600

    I swear I'm not this dumb all the time

--- pgdog/src/frontend/router/parser/rewrite/statement/update.rs
+++ pgdog/src/frontend/router/parser/rewrite/statement/update.rs
@@ -284,7 +284,7 @@ impl Inner {
     #[cfg(feature = "new_parser")]
     /// Do we have to return the rows to the client?
     pub(crate) fn is_returning(&self) -> bool {
-        self.from_update.returning_clause().is_none()
+        self.from_update.returning_clause().is_some()
     }
 
     #[cfg(not(feature = "new_parser"))]
