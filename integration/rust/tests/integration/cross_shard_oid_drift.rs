d2b3401c11324027e7f1564924acdd2103df9236
Sage Griffin <sage@sagetheprogrammer.com>
2026-08-03 10:21:50 -0600

    Add an integration test for OID drift on simple query protocol

--- integration/rust/tests/integration/cross_shard_oid_drift.rs
+++ integration/rust/tests/integration/cross_shard_oid_drift.rs
@@ -1,6 +1,7 @@
 #![cfg(feature = "new_parser")]
-use crate::setup::{admin_sqlx, connections_sqlx};
+use crate::setup::{admin_sqlx, admin_tokio, connections_sqlx, connections_tokio};
 use sqlx::Executor;
+use tokio_postgres::SimpleQueryMessage;
 
 #[derive(sqlx::Type, Debug, Clone, PartialEq)]
 #[sqlx(type_name = "test_oid_drift_composite")]
@@ -51,4 +52,62 @@ async fn test_oid_drift() {
         .await
         .unwrap();
     assert_eq!(rows, vec![composite; 20]);
+}
+
+#[tokio::test]
+async fn test_oid_drift_simple_protocol() {
+    let conn = connections_tokio().await.pop().unwrap();
+
+    // Intentionally cause the OID of the type to differ between shards
+    conn.simple_query("/* pgdog_shard: 0 */ CREATE SEQUENCE foo; DROP SEQUENCE foo;")
+        .await
+        .unwrap();
+    conn.simple_query("DROP TYPE IF EXISTS test_oid_drift_simple_composite CASCADE")
+        .await
+        .unwrap();
+    conn.simple_query("CREATE TYPE test_oid_drift_simple_composite AS (a text, b text)")
+        .await
+        .unwrap();
+    conn.simple_query("DROP TABLE IF EXISTS test_oid_drift_simple")
+        .await
+        .unwrap();
+    conn.simple_query(
+        "CREATE TABLE test_oid_drift_simple (customer_id BIGINT, composite test_oid_drift_simple_composite)",
+    )
+    .await
+    .unwrap();
+    admin_tokio().await.simple_query("RELOAD").await.unwrap();
+
+    let composite = Composite {
+        a: String::from("b"),
+        b: String::from("c"),
+    };
+    for i in 1..=20 {
+        conn.simple_query(&format!(
+                "INSERT INTO test_oid_drift_simple VALUES ({}, ROW('{}', '{}')::test_oid_drift_simple_composite)",
+                i,
+                composite.a,
+                composite.b,
+            ))
+            .await
+            .unwrap();
+    }
+
+    let messages = conn
+        .simple_query("SELECT composite FROM test_oid_drift_simple")
+        .await
+        .unwrap();
+    assert_eq!(messages.len(), 22);
+    let rows = messages
+        .iter()
+        .skip(1) // RowDescription
+        .take(20) // CommandComplete at end
+        .map(|message| {
+            let SimpleQueryMessage::Row(row) = message else {
+                panic!("not a DataRow: {:?}", message);
+            };
+            row.get(0).unwrap()
+        })
+        .collect::<Vec<_>>();
+    assert_eq!(rows, vec!["(b,c)"; 20]);
 }
