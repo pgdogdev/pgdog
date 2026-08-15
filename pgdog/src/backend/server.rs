f0df3ca55afeecc47d29a858f7927780708efdc4
Lev Kokotov <lev.kokotov@gmail.com>
2026-08-15 14:31:49 -0700

    save

--- pgdog/src/backend/server.rs
+++ pgdog/src/backend/server.rs
@@ -1326,7 +1326,7 @@ impl Drop for Server {
 pub mod test {
     use std::time::SystemTime;
 
-    use bytes::{BufMut, BytesMut};
+    use bytes::{BufMut, Bytes, BytesMut};
     use pgdog_stats::PreparedStatementsConfig;
     use tokio::{
         io::{AsyncReadExt, AsyncWriteExt},
@@ -2312,83 +2312,83 @@ pub mod test {
         assert!(!server.has_more_messages());
     }
 
-    // #[tokio::test]
-    // async fn test_manual_prepared() {
-    //     let mut server = test_server().await;
+    #[tokio::test]
+    async fn test_manual_prepared() {
+        crate::logger();
+        let mut server = test_server().await;
 
-    //     let mut prep = PreparedStatements::new();
-    //     let mut parse = Parse::named("test", "SELECT 1::bigint");
-    //     prep.insert_prepare(&mut parse);
-    //     assert_eq!(parse.name(), "__pgdog_1");
+        let mut prep = PreparedStatements::new();
+        let name = "test";
+        let query = Bytes::from("SELECT 1::bigint".to_owned());
+        let name = prep.insert_prepare(name, query.clone(), vec![]);
+        assert_eq!(name, "__pgdog_1");
 
-    //     server
-    //         .send(
-    //             &vec![ProtocolMessage::from(Query::new(format!(
-    //                 "PREPARE {} AS {}",
-    //                 parse.name(),
-    //                 parse.query()
-    //             )))]
-    //             .into(),
-    //         )
-    //         .await
-    //         .unwrap();
-    //     for c in ['C', 'Z'] {
-    //         let msg = server.read().await.unwrap();
-    //         assert_eq!(msg.code(), c);
-    //     }
-    //     assert!(server.sync_prepared());
-    //     server.sync_prepared_statements().await.unwrap();
-    //     assert!(server.prepared_statements.contains("__pgdog_1"));
+        server
+            .send(
+                &vec![ProtocolMessage::Query(Query::new(
+                    "PREPARE __pgdog_1 AS SELECT 1::bigint",
+                ))]
+                .into(),
+            )
+            .await
+            .unwrap();
+        for c in ['C', 'Z'] {
+            let msg = server.read().await.unwrap();
+            assert_eq!(msg.code(), c);
+        }
+        assert!(server.sync_prepared());
+        server.sync_prepared_statements().await.unwrap();
+        assert!(server.prepared_statements.contains("__pgdog_1"));
 
-    //     let describe = Describe::new_statement("__pgdog_1");
-    //     let bind = Bind::new_statement("__pgdog_1");
-    //     let execute = Execute::new();
-    //     server
-    //         .send(
-    //             &vec![
-    //                 describe.clone().into(),
-    //                 bind.into(),
-    //                 execute.into(),
-    //                 ProtocolMessage::from(Sync),
-    //             ]
-    //             .into(),
-    //         )
-    //         .await
-    //         .unwrap();
+        let describe = Describe::new_statement("__pgdog_1");
+        let bind = Bind::new_statement("__pgdog_1");
+        let execute = Execute::new();
+        server
+            .send(
+                &vec![
+                    describe.clone().into(),
+                    bind.into(),
+                    execute.into(),
+                    ProtocolMessage::from(Sync),
+                ]
+                .into(),
+            )
+            .await
+            .unwrap();
 
-    //     for c in ['t', 'T', '2', 'D', 'C', 'Z'] {
-    //         let msg = server.read().await.unwrap();
-    //         assert_eq!(c, msg.code());
-    //     }
+        for c in ['t', 'T', '2', 'D', 'C', 'Z'] {
+            let msg = server.read().await.unwrap();
+            assert_eq!(c, msg.code());
+        }
 
-    //     let parse = Parse::named("__pgdog_1", "SELECT 2::bigint");
-    //     let describe = describe.clone();
+        let parse = Parse::named("__pgdog_1", "SELECT 2::bigint");
+        let describe = describe.clone();
 
-    //     server
-    //         .send(&vec![parse.into(), describe.into(), ProtocolMessage::from(Flush)].into())
-    //         .await
-    //         .unwrap();
+        server
+            .send(&vec![parse.into(), describe.into(), ProtocolMessage::from(Flush)].into())
+            .await
+            .unwrap();
 
-    //     for c in ['1', 't', 'T'] {
-    //         let msg = server.read().await.unwrap();
-    //         assert_eq!(msg.code(), c);
-    //     }
+        for c in ['1', 't', 'T'] {
+            let msg = server.read().await.unwrap();
+            assert_eq!(msg.code(), c);
+        }
 
-    //     server
-    //         .send(&vec![ProtocolMessage::from(Query::new("EXECUTE __pgdog_1"))].into())
-    //         .await
-    //         .unwrap();
-    //     for c in ['T', 'D', 'C', 'Z'] {
-    //         let msg = server.read().await.unwrap();
-    //         assert_eq!(c, msg.code());
-    //         if c == 'D' {
-    //             let data_row = DataRow::from_bytes(msg.to_bytes()).unwrap();
-    //             let result: i64 = data_row.get(0, Format::Text).unwrap();
-    //             assert_eq!(result, 1); // We prepared SELECT 1, SELECT 2 is ignored.
-    //         }
-    //     }
-    //     assert!(server.done());
-    // }
+        server
+            .send(&vec![ProtocolMessage::from(Query::new("EXECUTE __pgdog_1"))].into())
+            .await
+            .unwrap();
+        for c in ['T', 'D', 'C', 'Z'] {
+            let msg = server.read().await.unwrap();
+            assert_eq!(c, msg.code());
+            if c == 'D' {
+                let data_row = DataRow::from_bytes(msg.to_bytes()).unwrap();
+                let result: i64 = data_row.get(0, Format::Text).unwrap();
+                assert_eq!(result, 1); // We prepared SELECT 1, SELECT 2 is ignored.
+            }
+        }
+        assert!(server.done());
+    }
 
     #[tokio::test]
     async fn test_sync_params() {
