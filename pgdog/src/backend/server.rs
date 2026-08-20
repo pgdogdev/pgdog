f60ab695f471adcc7f5b596a6f2fd6953b849dce
Lev Kokotov <lev.kokotov@gmail.com>
2026-08-20 12:32:07 -0700

    feat(tls): server certificate SAN can have a trailing dot

--- pgdog/src/backend/server.rs
+++ pgdog/src/backend/server.rs
@@ -279,7 +279,9 @@ impl Server {
                 )?;
                 let plain = stream.take()?;
 
-                let server_name = ServerName::try_from(addr.host.clone())?;
+                let server_name = ServerName::try_from(
+                    addr.host.strip_suffix('.').unwrap_or(&addr.host).to_owned(),
+                )?;
                 debug!("connecting with TLS to server name: {:?}", server_name);
 
                 match connector.connect(server_name.clone(), plain).await {
