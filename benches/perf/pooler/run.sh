7ad33d5ad5aedd3536090ceb33576684ef17a3db
jkaczman <jkzgit@pm.me>
2026-09-22 16:51:36 -0400

    ci: bencher v1, benches/perf/{lb,sharding,pooler}, error on perf regressions

--- benches/perf/pooler/run.sh
+++ benches/perf/pooler/run.sh
@@ -8,4 +8,4 @@ export PGHOST=127.0.0.1
 export PGPORT=6432
 
 pgbench -i
-pgbench -c 10 -j 2 -t 10000000 -f ${SCRIPT_DIR}/../select_1.sql -P 1
+pgbench -c 10 -j 2 -T 30 -f ${SCRIPT_DIR}/../select_1.sql -P 1
